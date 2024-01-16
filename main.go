1d0ce312e571ba09d284cc15152fb6add3c8f31d
Kevin Saucedo <developer2@sfperusac.com>
2024-01-16 15:41:06 -0500

    xenv cli comand

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+//go:embed version
+var version string
+
+func main() {
+	if len(os.Args) == 2 {
+		if os.Args[1] == "version" || os.Args[1] == "--version" {
+			fmt.Println(path.Base(os.Args[0]), version)
+			os.Exit(0)
+		}
+	}
+	if len(os.Args) > 2 {
+		slog.Error("to many args found", "args", strings.Join(os.Args[1:], " "))
+	}
+	var filename string
+	if len(os.Args) == 1 {
+		filename = ".env"
+	}
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
+	defer f.Close()
+	envs, err := godotenv.Parse(f)
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
+	var builder strings.Builder
+	for k, v := range envs {
+		builder.WriteString(fmt.Sprintf(`%s="%s" `, k, v))
+	}
+	fmt.Print(builder.String())
+}
